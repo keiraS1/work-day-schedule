f43d8fccd3c5e11d2c93c0326417a7f79ea22e39
keiraS1 <ktsamant0411@gmail.com>
2023-09-10 12:22:17 -0400

    finished color coding & getting localstorage onto browser webpage

--- script.js
+++ script.js
@@ -1,88 +1,62 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 //once browser has finished running (this includes CSS and HTML properties), JS will execute
-console.log("this works");
-
-
-
+// console.log("this works");
 $(function () {
+  // console.log(document.querySelector('.hour'))
+  var myKey = 'calendar-list';
 
-  // var hourOfDay = $(this).parent().attr("id");
-  // var calendarList = $(this).siblings(".description").val();
-  
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
+  function loadData() {
+    // the present hour- not displayed but stored for use when comparing times of day and color coding the calendar
+    var currentTime = dayjs().hour();
 
- 
-  
+    $(".description").each(function () {
+      var container = $(this).parent();
+      var timeOfDay = container.attr("id");
+      // console.log({ timeOfDay });
+      $(this).val(calendar[timeOfDay])
+//if the id - converted into a number matches the currenttime-hour then red textarea
+      if (+timeOfDay === currentTime) {
+        container.addClass('present')
+//if id number is a smaller number than the number of the currenttime - grey textarea
+      } else if (+timeOfDay < currentTime) {
+        container.addClass('past')
+//if id number is greater than the number of the currenttime - green area
+      } else {
+        container.addClass('future')
+      }
+    })
+  }
+//getting the data FROM localstorage
+  function getData() {
+    var data = JSON.parse(localStorage.getItem(myKey));
+    if (!data) {
+      data = {}
+      saveData(data);
+    }
+    return data
+  }
+//storing the data INTO local storage
+  function saveData(data) {
+    return localStorage.setItem(myKey, JSON.stringify(data));
+  }
 
-  //HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // var currentHour = today.getHours();
-//
-function timeOfDayTracker (){
-
-var time = dayjs().hour();
-console.log(time);
-
- var timeBlock = $(".time-block");
- timeBlock.each(function(){
-  
- })
-
-
-}
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
-   // var saveBtn = $(".saveBtn");
-   $(".saveBtn").on("click", function(){
-    console.log("this is working")
+  var calendar = getData()
+//click event so that when textarea is filled, clicking save button stores the string into localstorage
+  $(".saveBtn").on("click", function () {
+    console.log("this works")
     alert("Your task has been saved!");
-    // console.log(this);
-//     var CalendarPlans = {
-//       dayTime: hourOfDay,
-//       calendar: calendarList
+    // so that we do not repeat multiple times over and over we are using this as/in each "save" button and storing the parent- the div-id of the hour into local storage
+    var hourOfDay = $(this).parent().attr("id");
+    //we will also stored the text content that will be stored int he description class
+    var tasks = $(this).siblings(".description").val();
 
-// };
-    // so that we do not repeat multiple times over and over we are using this as in each save button and storing the parent- the div-id of the hour into local storage
-  var hourOfDay = $(this).parent().attr("id");
-  var calendarList = $(this).siblings(".description").val();
-
-    // console.log(this);
-    localStorage.setItem(hourOfDay,JSON.stringify(calendarList));
-    // console.log(calendarList);
-
-  //now to create getItem so information stays on page
-
-
-
-
- 
+    calendar[hourOfDay] = tasks;
+    saveData(calendar)
   })
+//calling the function of loading the data onto the browser -color coding the calendar
+  loadData();
 
-  
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
-  //adds current day
+  //adds current day/ updates each day automatically as the days change
   var currentDate = dayjs();
   $("#today").text(currentDate.format("MMM D YYYY"))
-  
 
 });
